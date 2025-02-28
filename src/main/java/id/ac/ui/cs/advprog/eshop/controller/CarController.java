e6099a079894b5863f4492a02466f3479ebee547
akhyarrasyid <akhyarrasyid2005@gmail.com>
2025-02-28 15:00:44 +0700

    feat: Add validation for car

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -1,16 +1,13 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
 import java.util.List;
+import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
@@ -29,7 +26,10 @@ class CarController extends ProductController {
     }
 
     @PostMapping("/createCar")
-    public String createCarPost (@ModelAttribute Car car, Model model) {
+    public String createCarPost (@Valid @ModelAttribute Car car, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "createCar";
+        }
         carService.create(car);
         return "redirect:listCar";
     }
@@ -49,7 +49,10 @@ class CarController extends ProductController {
     }
 
     @PostMapping("/editCar")
-    public String editCarPost (@ModelAttribute Car car, Model model) {
+    public String editCarPost (@Valid @ModelAttribute Car car, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "editCar";
+        }
         carService.update(car.getCarId(), car);
         return "redirect:listCar";
     }
